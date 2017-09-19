bc540300135de1d4145933f58345b31ea508a7e5
Alvaro Fernandez <nishinishi9999@gmail.com>
2017-09-19 18:39:41 +0200

    first commit

---
+++ server/server.js
@@ -0,0 +1,44 @@
+﻿const WSS = require('ws').Server;
+
+
+var wss = new WSS({ port: 8080 });
+var counter = 1;
+
+wss.on('connection', function(socket)
+    {
+        console.log('クライアントに接続しました');
+        
+        /****************************
+        * Send number on connection
+        ****************************/
+        var json = JSON.stringify({ n: 1234 });
+        socket.send(json);
+        
+        socket.on('message', function(msg)
+            {
+                console.log('メッセージ: ', msg);
+            });
+        
+        socket.on('close', function()
+            {
+                console.log('クライアントと切断しました');
+            });
+    });
+
+
+function broadcast()
+    {
+        var json = JSON.stringify({ n: counter++ });
+        
+        for(var i = 0; i < wss.clients.length; i++)
+            {
+                var client = wss.clients[i];
+                
+                client.send(json);
+                console.log('Sent: ', json);
+            }
+        
+        console.log(1);
+    }
+
+setInterval(broadcast, 5000);
