4f1a933ad1d6b359c0975cc916831fefad45e7bb
Alvaro Fernandez <nishinishi9999@gmail.com>
2017-09-20 18:48:05 +0200

    push

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,6 @@ var express = require('express');
 var router  = express.Router();
 
 
-var WS_PORT = normalizePort(process.env.PORT || '8080' || '3000');
-
-
 router.get('/pagecount', function(req, res, next)
     {
         res.send(true);
