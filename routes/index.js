a7bc69ee60bb251ea17f4a1e851e06e98035c50f
Alvaro Fernandez <nishinishi9999@gmail.com>
2017-09-20 13:06:01 +0200

    push

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/pagecount', function(req, res, next)
+    {
+        res.send(true);
+    });
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile('./public/index.html');
-});
+router.get('/', function(req, res, next)
+    {
+        res.render('index', { title: 'Express' });
+    });
 
 module.exports = router;
